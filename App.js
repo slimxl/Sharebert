0eb00b24cfc91998a3469bc93e1dc1a102c7a56a
Eric <eric@sharebert.com>
2018-01-25 19:17:09 -0500

    Swipe Function works

--- App.js
+++ App.js
@@ -1,57 +1,9 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'HELLO WORLD GARY,\n' +
-    '',
-});
-
-export default class App extends Component<{}> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+import { AppRegistry } from 'react-native'
+import Basic from './components/Basic/'
+// import Dynamic from './components/Dynamic/'
+// import LoadMinimal from './components/LoadMinimal/'
+// import Phone from './components/Phone/'
+// import PhotoView from './components/PhotoView/' // not working
+// import Swiper from './components/Swiper/'  // working but no title displayed
+// import SwiperNumber from './components/SwiperNumber/' // working but no title displayed
+AppRegistry.registerComponent('myproject', () => Basic);
