689cc049cc05f78aeaedb1957e3e0769786d929c
Eric <eric@sharebert.com>
2018-03-20 16:03:14 -0400

    Tab navigator

--- Sharebert/Explore.js
+++ Sharebert/Explore.js
@@ -8,6 +8,7 @@ import {
   StyleSheet,
   AsyncStorage,
   View,
+  FlatList,
   Text,
   ScrollView,
   Alert,
@@ -20,7 +21,7 @@ import {
   TouchableOpacity,
   TextInput,
 } from 'react-native';
-
+import { TabNavigator } from 'react-navigation';
 import { Constants } from 'expo';
 const { width } = 10;
 var userPoints = 0;
@@ -815,15 +816,11 @@ class Explore extends Component {
   };
 
   render() {
-    const menu = <Menu onItemSelected={this.onMenuItemSelected} />;
-    
+   
     try {
       return (
         <View style={styles.container}>
-          <SideMenu
-            menu={menu}
-            isOpen={this.state.isOpen}
-            onChange={isOpen => this.updateMenuState(isOpen)}>
+        
             <Swiper
               style={styles.swiper}
               ref={swiper => {
@@ -1186,8 +1183,6 @@ class Explore extends Component {
                   style={styles.button2}
                 />
             </TouchableOpacity>
-
-          </SideMenu>
           
         </View>
       );
@@ -1196,7 +1191,218 @@ class Explore extends Component {
     }
   }
 }
+var userPoints = 0;
+var userID = 0;
+var like;
+var like2;
+class Likes extends Component {
+  constructor(props) {
+    super(props);
+    userID = this.props.navigation.state.params.id;
+    userPoints = this.props.navigation.state.params.points;
+    if (userPoints === undefined || userID === undefined) {
+      userPoints = 0;
+      userID = 0;
+    }
+    this.state = {
+      isOpen: false,
+      selectedItem: 'Likes',
+      userPoints: userPoints,
+      userID: userID,
+    };
+    this.getFile();
+  }
 
+  onMenuItemSelected = item => {
+    this.setState({
+      isOpen: false,
+      selectedItem: item,
+    });
+    if (item === 'Likes') {
+      this.props.navigation.navigate('Likes');
+    } else if (item === 'Explore') {
+      this.props.navigation.navigate('Explore');
+    }
+  };
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  }
+
+  updateMenuState(isOpen) {
+    this.setState({ isOpen });
+  }
+
+  shareURL(item) {
+    try {
+      Share.share(
+        {
+          ...Platform.select({
+            ios: {
+              url: item.URL,
+            },
+            android: {
+              message: 'Look at this : \n' +
+                item.URL,
+            },
+          }),
+          title: 'Wow, did you see that?',
+        },
+        {
+          ...Platform.select({
+            ios: {
+              // iOS only:
+              excludedActivityTypes: ['com.apple.UIKit.activity.PostToTwitter'],
+            },
+            android: {
+              // Android only:
+              dialogTitle: 'Share : ' +
+                item.Title,
+            },
+          }),
+        }
+      ).then(({ action, activityType }) => {
+        if (action === Share.dismissedAction) {
+          Alert.alert("Hey!", "Don't Forget, You Get Points for Sharing Products!");
+        }
+        else {
+          try {
+            if (userID != 0) {
+              fetch(
+                'https://sharebert.com/DBAwardPoints.php?uid=' +
+                userID +
+                '&type=1',
+                { method: 'GET' }
+              )
+                .then(response2 => response2.json())
+                .then(responseData2 => {
+                  if (responseData2['Points'] != userPoints) {
+
+                    userPoints = responseData2['Points'];
+                    Alert.alert('POINTS OBTAINED', "Thanks for Sharing!");
+
+                  }
+                })
+                .done();
+            }
+          } catch (error) {
+            console.error(error);
+          }
+        }
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  _onPress(item) {
+    Alert.alert(
+      'Buy Or Share',
+      item.Title,
+      [
+        {
+          text: 'Cancel',
+          onPress: () => console.log('Cancel Pressed'),
+          style: 'cancel',
+        },
+        { text: 'Share', onPress: () => this.shareURL(item) },
+        { text: 'Buy', onPress: () => { Linking.openURL(item.URL); } },
+
+      ],
+      { cancelable: false }
+    );
+  }
+
+  getFile = async (type) => {
+    try {
+      var likesave;
+      if (userID !== undefined) {
+        likesave = await AsyncStorage.getItem('@MySuperStore:Likes' + userID);
+      }
+      else {
+        likesave = await AsyncStorage.getItem('@MySuperStore:Likes');
+      }
+      if (likesave !== null) {
+        // We have data!!
+        like2 = JSON.parse(likesave);
+        like = like2.filter(function (n) { return n });
+      }
+      else {
+        like = null;
+      }
+      this.forceUpdate();
+    } catch (error) {
+      // Error retrieving data
+    }
+  }
+
+  render() {
+    return (
+      <View style={styless.container}>
+
+        <TouchableOpacity
+          onPress={() => {
+            this.props.navigation.dispatch(this.props.navigation.navigate('Explore', {
+              id: userID,
+              points: userPoints,
+            }));
+          }}>
+
+          <Image style={styless.header} />
+          <Text style={styless.headertext}>
+            Tap to Explore
+              </Text>
+        </TouchableOpacity>
+        <Image
+          resizeMode="contain"
+          style={styless.button}
+          source={require('./Logo.png')}
+        />
+        <TouchableOpacity
+          onPress={() => {
+            //this.props.navigation.dispatch(backAction); //navigate to explore
+
+            this.props.navigation.dispatch(this.props.navigation.navigate('Explore', {
+              id: userID,
+              points: userPoints,
+            }));
+          }}>
+          <Image
+            style={styless.hamburger}
+            source={require('./purplemenuicon.png')}
+          />
+        </TouchableOpacity>
+
+        <Text style={styless.title}>
+          Things You Like
+        </Text>
+        <FlatList
+          data={like}
+          keyExtractor={(item, index) => index}
+          renderItem={({ item, separators }) => (
+            <TouchableOpacity
+              onPress={() => this._onPress(item)}
+              onShowUnderlay={separators.highlight}
+              onHideUnderlay={separators.unhighlight}>
+              <View style={{ backgroundColor: 'white' }}>
+                <Text style={styless.text}>{item.Title}</Text>
+                <Image
+                  resizeMode={'contain'}
+                  style={styless.image}
+                  source={{
+                    uri: item.ImageURL,
+                  }}
+                />
+              </View>
+            </TouchableOpacity>
+          )}
+        />
+      </View>
+    );
+  }
+}
 const styles = StyleSheet.create({
   container: {
     marginTop: Constants.statusBarHeight,
@@ -1337,6 +1543,87 @@ const styles = StyleSheet.create({
     height: 25,
   },
 });
+const styless = StyleSheet.create({
+  container: {
+    ...Platform.select({
+      ios: {
+        marginTop: Constants.statusBarHeight,
+      },
+      android: {
+        marginTop: 80,
+      },
+    }),
+
+    flex: 1,
+    backgroundColor: '#F5FCFF',
+  },
+  hamburger: {
+    ...Platform.select({
+      ios: {
+        marginTop: -47,
+      },
+      android: {
+        marginTop: -10,
+      },
+    }),
+    width: 30,
+    height: 23,
+    marginLeft: 10,
+    
+    backgroundColor: 'transparent',
+    padding: 0,
+  },
+  title: {
+    fontFamily: "Montserrat",
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#ff2eff',
+    marginTop: -15,
+  },
+  image: {
+    
+    width: 100,
+    height: 100,
+    marginTop: -50,
+  },
+  button: {
+    width: 100,
+    height: 70,
+    marginTop: -44,
+    marginLeft: 60,
+    backgroundColor: 'transparent',
+    padding: 20,
+  },
+  header: {
+    ...Platform.select({
+      ios: {
+        marginTop: 0,
+      },
+      android: {
+        marginTop: -10,
+      },
+    }),
+    width: '100%',
+    height: 40,
+    backgroundColor: '#dee6ee',
+  },
+  text: {
+    textAlign: 'left',
+    fontSize: 12,
+    marginTop: 40,
+    marginLeft: 110,
+    backgroundColor: 'transparent',
+  },
+  headertext:
+    {
+      textAlign: 'center',
+      fontSize: 17,
+      marginTop: -32,
+      marginLeft: 130,
+      backgroundColor: 'transparent',
+    },
+});
 
 function shuffle(array) {
   var currentIndex = array.length, temporaryValue, randomIndex;
@@ -1353,5 +1640,8 @@ function shuffle(array) {
 
   return array;
 }
-export default Explore;
+export default TabNavigator({
+  Explore: { screen: Explore },
+  Likes: { screen: Likes },
+});
 export { likes };
