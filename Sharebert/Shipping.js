4d2a95b3a5119f19498e93dadb8f6b8d9c1a7e94
JLathey <jalathey@fullsail.edu>
2018-04-09 14:32:47 -0400

    Cleaned up Shipping confirmation alert

--- Sharebert/Shipping.js
+++ Sharebert/Shipping.js
@@ -164,10 +164,8 @@ class Shipping extends Component {
     }
     if (userID != 0) {
       Alert.alert("User Data Confirmation",
-        +userID + " \n"
         + user.Name + " \n"
         + user.Phone + " \n"
-        + user.Email + " \n"
         + user.Address + " \n"
         + user.State + " \n"
         + user.Zip + " \n",
