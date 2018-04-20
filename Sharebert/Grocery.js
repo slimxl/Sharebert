4c1d6bdf511b66b6a6e419766a7618418123d8e1
JLathey <jalathey@fullsail.edu>
2018-04-20 03:39:14 -0400

    populated grocery page

--- Sharebert/Grocery.js
+++ Sharebert/Grocery.js
@@ -49,134 +49,89 @@ class Grocery extends Component {
 
 
     var Brand1 = {};
-    Brand1['Term'] = 'Tripping';
-    Brand1['title'] = 'TrippingGroceryTest';
-    Brand1['link'] = 'http://www.dpbolvw.net/click-8331277-13174391';
-    Brand1['image_url'] = require('./assets/travel/tripping.png');
+    Brand1['Term'] = 'amazonfresh';
+    Brand1['title'] = 'Amazon Fresh';
+    Brand1['link'] = 'https://amzn.to/2F1coZe';
+    Brand1['image_url'] = require('./assets/grocery/amazonfresh.jpg');
 
     var Brand2 = {};
-    Brand2['Term'] = 'Travelocity';
-    Brand2['title'] = 'Travelocity';
-    Brand2['link'] = 'http://www.tkqlhce.com/click-8331277-10842219';
-    Brand2['image_url'] = require('./assets/travel/travelocity.png');
+    Brand2['Term'] = 'bbqbox';
+    Brand2['title'] = 'BBQ Box';
+    Brand2['link'] = 'https://www.shareasale.com/r.cfm?b=925691&u=1404632&m=68335';
+    Brand2['image_url'] = require('./assets/grocery/bbqbox.jpg');
 
     var Brand3 = {};
-    Brand3['Term'] = 'One Travel';
-    Brand3['title'] = 'One Travel';
-    Brand3['link'] = 'http://www.pntrac.com/t/8-206-163999-1508';
-    Brand3['image_url'] = require('./assets/travel/onetravel.png');
+    Brand3['Term'] = 'craft beer club';
+    Brand3['title'] = 'Craft Beer Club';
+    Brand3['link'] = 'https://www.shareasale.com/r.cfm?b=278781&u=1404632&m=30876';
+    Brand3['image_url'] = require('./assets/grocery/craftbeerclub.jpg');
 
     var Brand4 = {};
-    Brand4['Term'] = 'CheapAir';
-    Brand4['title'] = 'CheapAir';
-    Brand4['link'] = 'http://www.anrdoezrs.net/click-8331277-11014261';
-    Brand4['image_url'] = require('./assets/travel/cheapair.png');
+    Brand4['Term'] = 'foodstirs';
+    Brand4['title'] = 'Foodstirs';
+    Brand4['link'] = 'https://click.linksynergy.com/fs-bin/click?id=MIcOmftyarA&offerid=568140.62&type=3&subid=0';
+    Brand4['image_url'] = require('./assets/grocery/foodstirs.jpg');
 
     var Brand5 = {};
-    Brand5['Term'] = 'CheapOair';
-    Brand5['title'] = 'CheapOair';
-    Brand5['link'] = 'http://www.jdoqocy.com/click-8331277-12953065';
-    Brand5['image_url'] = require('./assets/travel/cheapoair.png');
+    Brand5['Term'] = 'fossilfarms';
+    Brand5['title'] = 'Fossil Farms';
+    Brand5['link'] = 'https://www.shareasale.com/r.cfm?b=80824&u=1404632&m=12637';
+    Brand5['image_url'] = require('./assets/grocery/fossilfarms.jpg');
 
     var Brand6 = {};
-    Brand6['Term'] = 'CheapTickets';
-    Brand6['title'] = 'CheapTickets';
-    Brand6['link'] = 'http://www.kqzyfj.com/click-8331277-12843040';
-    Brand6['image_url'] = require('./assets/travel/cheaptickets.png');
+    Brand6['Term'] = 'goldmedalwineclub';
+    Brand6['title'] = 'Gold Medal Wine Club';
+    Brand6['link'] = 'https://www.shareasale.com/r.cfm?b=194707&u=1404632&m=24293';
+    Brand6['image_url'] = require('./assets/grocery/goldmedalwineclub.jpg');
 
     var Brand7 = {};
-    Brand7['Term'] = 'Extended Stay America';
-    Brand7['title'] = 'Extended Stay America';
-    Brand7['link'] = 'http://www.jdoqocy.com/click-8331277-13107969';
-    Brand7['image_url'] = require('./assets/travel/extended.png');
+    Brand7['Term'] = 'hellofresh';
+    Brand7['title'] = 'Hello Fresh';
+    Brand7['link'] = 'http://www.jdoqocy.com/click-8331277-11802340';
+    Brand7['image_url'] = require('./assets/grocery/hellofresh.jpg');
 
     var Brand8 = {};
-    Brand8['Term'] = 'Homestay';
-    Brand8['title'] = 'Homestay';
-    Brand8['link'] = 'http://www.kqzyfj.com/click-8331277-12353257';
-    Brand8['image_url'] = require('./assets/travel/homestay.png');
+    Brand8['Term'] = 'houseofglunz';
+    Brand8['title'] = 'House of Glunz';
+    Brand8['link'] = 'https://www.shareasale.com/r.cfm?b=953933&u=1404632&m=69358';
+    Brand8['image_url'] = require('./assets/grocery/houseofglunz.jpg');
 
     var Brand9 = {};
-    Brand9['Term'] = 'HotelPlanner.com';
-    Brand9['title'] = 'HotelPlanner.com';
-    Brand9['link'] =  'http://www.jdoqocy.com/click-8331277-10391851';
-    Brand9['image_url'] = require('./assets/travel/hotelplanner.png');
+    Brand9['Term'] = 'urthbox';
+    Brand9['title'] = 'Urthbox';
+    Brand9['link'] =  'https://www.shareasale.com/r.cfm?b=491113&u=1404632&m=47975';
+    Brand9['image_url'] = require('./assets/grocery/urthbox.jpg');
 
     var Brand10 = {};
-    Brand10['Term'] = 'Hotels.com';
-    Brand10['title'] = 'Hotels.com';
-    Brand10['link'] = 'http://www.anrdoezrs.net/click-8331277-10443216';
-    Brand10['image_url'] = require('./assets/travel/hotels.png');
+    Brand10['Term'] = 'veestro';
+    Brand10['title'] = 'Veestro';
+    Brand10['link'] = 'https://www.shareasale.com/r.cfm?b=517341&u=1404632&m=49480';
+    Brand10['image_url'] = require('./assets/grocery/veestro.jpg');
 
     var Brand11 = {};
-    Brand11['Term'] = 'LivingSocial';
-    Brand11['title'] = 'LivingSocial';
-    Brand11['link'] = 'http://www.anrdoezrs.net/click-8331277-11659343';
-    Brand11['image_url'] = require('./assets/travel/livingsocial.png');
+    Brand11['Term'] = 'winc';
+    Brand11['title'] = 'winc';
+    Brand11['link'] = 'https://www.shareasale.com/r.cfm?b=792888&u=1404632&m=62347';
+    Brand11['image_url'] = require('./assets/grocery/winc.jpg');
 
     var Brand12 = {};
-    Brand12['Term'] = 'Otel.com';
-    Brand12['title'] = 'Otel.com';
-    Brand12['link'] = 'http://www.anrdoezrs.net/click-8331277-10784091';
-    Brand12['image_url'] = require('./assets/travel/otel.png');
+    Brand12['Term'] = 'winecrasher';
+    Brand12['title'] = 'Winecrasher';
+    Brand12['link'] = 'https://www.shareasale.com/r.cfm?b=865646&u=1404632&m=65843';
+    Brand12['image_url'] = require('./assets/grocery/winecrasher.jpg');
 
     var Brand13 = {};
-    Brand13['Term'] = 'Palace Resorts';
-    Brand13['title'] = 'Palace Resorts';
-    Brand13['link'] = 'http://www.kqzyfj.com/click-8331277-13221537';
-    Brand13['image_url'] = require('./assets/travel/palaceresorts.png');
+    Brand13['Term'] = 'wineofthemonthclub';
+    Brand13['title'] = 'Wine of the Month Club';
+    Brand13['link'] = 'https://www.shareasale.com/r.cfm?b=486942&u=1404632&m=47747';
+    Brand13['image_url'] = require('./assets/grocery/wineofthemonthclub.jpg');
 
     var Brand14 = {};
-    Brand14['Term'] = 'Walt Disney Travel Company';
-    Brand14['title'] = 'Walt Disney Travel Company';
-    Brand14['link'] = 'http://www.tkqlhce.com/click-8331277-13265516';
-    Brand14['image_url'] = require('./assets/travel/disney.png');
+    Brand14['Term'] = 'wiredforwine';
+    Brand14['title'] = 'Wired for Wine';
+    Brand14['link'] = 'https://www.shareasale.com/r.cfm?b=211020&u=1404632&m=25630';
+    Brand14['image_url'] = require('./assets/grocery/wiredforwine.jpg');
 
-    var Brand15 = {};
-    Brand15['Term'] = 'Booking.com';
-    Brand15['title'] = 'Booking.com';
-    Brand15['link'] = 'https://click.linksynergy.com/fs-bin/click?id=MIcOmftyarA&offerid=343625.119&type=3&subid=';
-    Brand15['image_url'] = require('./assets/travel/booking.png');
-
-    var Brand16 = {};
-    Brand16['Term'] = 'Camping World';
-    Brand16['title'] = 'Camping World';
-    Brand16['link'] = 'https://click.linksynergy.com/fs-bin/click?id=MIcOmftyarA&offerid=414807.10002997&type=3&subid=';
-    Brand16['image_url'] = require('./assets/travel/campingworld.png');
-
-    var Brand17 = {};
-    Brand17['Term'] = 'CarRentals.com';
-    Brand17['title'] = 'CarRentals.com';
-    Brand17['link'] = 'https://click.linksynergy.com/fs-bin/click?id=MIcOmftyarA&offerid=453842.10000029&type=4&subid=';
-    Brand17['image_url'] = require('./assets/travel/carrentals.png');
-
-    var Brand18 = {};
-    Brand18['Term'] = 'LaQuinta';
-    Brand18['title'] = 'LaQuinta';
-    Brand18['link'] = 'https://click.linksynergy.com/fs-bin/click?id=MIcOmftyarA&offerid=53964.10000074&type=4&subid=';
-    Brand18['image_url'] = require('./assets/travel/laquinta.png');
-
-    var Brand19 = {};
-    Brand19['Term'] = 'Thrifty Rent-A-Car';
-    Brand19['title'] = 'Thrifty Rent-A-Car';
-    Brand19['link'] = 'https://click.linksynergy.com/fs-bin/click?id=MIcOmftyarA&offerid=171424.10000028&type=3&subid='
-    Brand19['image_url'] = require('./assets/travel/thrifty.png');
-
-    var Brand20 = {};
-    Brand20['Term'] = 'TicketCity';
-    Brand20['title'] = 'TicketCity';
-    Brand20['link'] = 'https://click.linksynergy.com/fs-bin/click?id=MIcOmftyarA&offerid=360735.376&type=4&subid=';
-    Brand20['image_url'] = require('./assets/travel/ticketcity.png');
-
-    var Brand21 = {};
-    Brand21['Term'] = 'Travelation';
-    Brand21['title'] = 'Travelation ';
-    Brand21['link'] = 'https://click.linksynergy.com/fs-bin/click?id=MIcOmftyarA&offerid=562139.573&type=4&subid=';
-    Brand21['image_url'] = require('./assets/travel/travelation.png');
-
-
-
-  
     
     if(brandArr.length<1)
     {
@@ -194,15 +149,6 @@ class Grocery extends Component {
       brandArr.push(Brand12);
       brandArr.push(Brand13);
       brandArr.push(Brand14);
-      brandArr.push(Brand15);
-      brandArr.push(Brand16);
-      brandArr.push(Brand17);
-      brandArr.push(Brand18);
-      brandArr.push(Brand19);
-      brandArr.push(Brand20);
-      brandArr.push(Brand21);
-
-
     }
     
     
