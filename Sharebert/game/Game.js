fcb375e91fec14f698034ef0f7de755b1120aee0
Eric <eric@sharebert.com>
2018-05-10 12:11:03 -0400

    audio fixes

--- Sharebert/game/Game.js
+++ Sharebert/game/Game.js
@@ -33,7 +33,7 @@ export default class Game extends React.Component {
   componentWillMount() {
     THREE.suppressExpoWarnings(true);
     /// Audio is currently broken in snack :/
-    // this.setupAudio();
+     this.setupAudio();
   }
 
   setupAudio = async () => {
@@ -255,7 +255,7 @@ export default class Game extends React.Component {
     if (!this.gameOver) {
       // @(Evan Bacon) These are in-game taps for making the bird flap
       this.velocity = FLAP;
-      // this.audio.wing();
+       this.audio.wing();
     } else {
       // @(Evan Bacon) This is an end-game tap to reset the game
       // fetch(
@@ -278,7 +278,7 @@ export default class Game extends React.Component {
   //@(Evan Bacon) Update the state with the new score so our React component knows to update... Then play cool noise!
   addScore = () => {
     this.setState({ score: this.state.score + 1 });
-    // this.audio.point();
+     this.audio.point();
   };
 
   //@(Evan Bacon) stop the pipe spawning and play that fresh slapping sound.
@@ -287,7 +287,7 @@ export default class Game extends React.Component {
 
     clearInterval(this.pillarInterval);
 
-    // this.audio.hit();
+     this.audio.hit();
   };
 
   //@(Evan Bacon) This is the clean state before each game.
